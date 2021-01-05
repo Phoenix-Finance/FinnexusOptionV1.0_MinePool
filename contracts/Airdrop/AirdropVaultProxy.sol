64fb4b2a818aac2867bf65b8f5406cc1a7c1bbab
jiaqinggang1 <forestjqg@126.com>
2021-01-05 06:27:57 -0800

    comment parameters name in proxy

--- contracts/Airdrop/AirdropVaultProxy.sol
+++ contracts/Airdrop/AirdropVaultProxy.sol
@@ -12,49 +12,42 @@ contract AirDropVaultProxy is AirDropVaultData,baseProxy {
     constructor (address implementation_) baseProxy(implementation_) public{
     }
     
-    function setOptionColPool(address _optionColPool) public  {
+    function setOptionColPool(address /*_optionColPool*/) public  {
         delegateAndReturn();
     }
     
-    function setMinePool( address _minePool) public  {
+    function setMinePool( address /*_minePool*/) public  {
        delegateAndReturn();
     }
     
-    function setFnxToken(address _fnxToken) public  {
+    function setFnxToken(address /*_fnxToken*/) public  {
         delegateAndReturn();
     }
    
-    function setFptbToken(address _ftpbToken) public  {
+    function setFptbToken(address /*_ftpbToken*/) public  {
         delegateAndReturn();
     }
     
-    function setTotalAirdropFnx(uint256 _totalAirdropFnx) public  {
+    function setTotalAirdropFnx(uint256 /*_totalAirdropFnx*/) public  {
         delegateAndReturn();
     }
 
-    function setFnxPerPerson(uint256 _fnxPerPerson)  public  {
+    function setFnxPerPerson(uint256 /*_fnxPerPerson*/)  public  {
         delegateAndReturn();
     }
     
-    /**
-     * @dev getting back the left mine token
-     * @param reciever the reciever for getting back mine token
-     */
-    function getbackLeftFnx(address reciever)  public  {
+    function getbackLeftFnx(address /*reciever*/)  public  {
         delegateAndReturn();
     }  
 
-    /**
-     * @dev Retrieve user's locked balance. 
-     * @param account user's account.
-     */ 
-    function balanceOfAirdrop(address account) public view returns (uint256) {
+
+    function balanceOfAirdrop(address /*account*/) public view returns (uint256) {
         delegateToViewAndReturn();   
     }
 
 
-    function addWhiteList(address account) external {
-        airDropWhiteList[account] = fnxPerPerson;
+    function addWhiteList(address /*account*/) external {
+        delegateAndReturn();
     }
     
     
