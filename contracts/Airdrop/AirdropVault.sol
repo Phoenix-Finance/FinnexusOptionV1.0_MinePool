7adcecc9f817cd16e6712e16106b9aa665420aac
jeffqg123 <forestjqg@126.com>
2021-01-15 12:42:51 +0800

    added sushi mine and free claimer support

--- contracts/Airdrop/AirdropVault.sol
+++ contracts/Airdrop/AirdropVault.sol
@@ -12,32 +12,71 @@ interface IMinePool {
     function lockAirDrop(address user,uint256 ftp_b_amount) external;
 }
 
+interface ITargetToken {
+     function balanceOf(address account) external view returns (uint256);
+}
+
+
 contract AirDropVault is AirDropVaultData {
     using SafeMath for uint256;
     
+    modifier airdropinited() {
+        require(optionColPool!=address(0),"collateral pool address should be set");
+        require(minePool!=address(0),"mine pool address should be set");
+        require(fnxToken!=address(0),"fnx token address should be set");
+        require(ftpbToken!=address(0),"ftpb token address should be set");
+        require(claimBeginTime>0,"airdrop claim begin time should be set");
+        require(claimEndTime>0,"airdrop claim end time should be set");
+        require(fnxPerFreeClaimUser>0,"the air drop number for each free claimer should be set");
+        require(minBalForFreeClaim>0,"the thredhold for free claimer should be set");
+        require(maxWhiteListFnxAirDrop>0,"the max fnx number for whitelist air drop should be set");
+        require(maxFreeFnxAirDrop>0,"the max fnx number for free air drop should be set");
+        _;
+    }
+    
+    modifier suhsimineinited() {
+        require(cfnxToken!=address(0),"cfnc token address should be set");
+        require(sushiMineStartTime>0,"sushi mine start time should be set");
+        require(sushimineInterval>0,"sushi mine interval should be set");
+        _;
+    }    
 
     function initialize() onlyOwner public {
     }
     
     function update() onlyOwner public{
+        
     }
     
-    function setOptionColPool(address _optionColPool) public onlyOwner {
+    function initAirdrop( address _optionColPool,
+                                address _minePool,
+                                address _fnxToken,
+                                address _ftpbToken,
+                                uint256 _claimBeginTime,
+                                uint256 _claimEndTime,
+                                uint256 _fnxPerFreeClaimUser,
+                                uint256 _minBalForFreeClaim,
+                                uint256 _maxFreeFnxAirDrop,
+                                uint256 _maxWhiteListFnxAirDrop) public onlyOwner {
         optionColPool = _optionColPool;
-    }
-    
-    function setMinePool( address _minePool) public onlyOwner {
         minePool = _minePool;
-    }
-    
-    function setFnxToken(address _fnxToken) public onlyOwner {
-        fnxToken = _fnxToken;
-    }
-   
-    function setFptbToken(address _ftpbToken) public onlyOwner {
+        fnxToken = _fnxToken;  
         ftpbToken = _ftpbToken;
+        claimBeginTime = _claimBeginTime;
+        claimEndTime = _claimEndTime;
+        fnxPerFreeClaimUser = _fnxPerFreeClaimUser;
+        minBalForFreeClaim = _minBalForFreeClaim;
+        maxFreeFnxAirDrop = _maxFreeFnxAirDrop;
+        maxWhiteListFnxAirDrop = _maxWhiteListFnxAirDrop;
     }
     
+    function initSushiMine(address _cfnxToken,uint256 _sushiMineStartTime,uint256 _sushimineInterval) public onlyOwner{
+        cfnxToken = _cfnxToken;
+        sushiMineStartTime = _sushiMineStartTime;
+        sushimineInterval = _sushimineInterval;
+    }
+    
+
     /**
      * @dev getting back the left mine token
      * @param _reciever the reciever for getting back mine token
@@ -51,37 +90,95 @@ contract AirDropVault is AirDropVaultData {
      * @dev Retrieve user's locked balance. 
      * @param _account user's account.
      */ 
-    function balanceOfAirdrop(address _account) public view returns (uint256) {
-        return airDropWhiteList[_account];
+    function balanceOfWhitListUser(address _account) public view returns (uint256) {
+        return userWhiteList[_account];
     }
 
 
-    function addWhiteList(address _account,uint256 _fnxnumber) public onlyOperator(1) {
-        airDropWhiteList[_account] = _fnxnumber;
-        totalAirdrop = totalAirdrop.add(_fnxnumber);
-        emit AddWhiteList(_account,_fnxnumber);
+    function addWhiteList(address[] memory _accounts,uint256[] memory _fnxnumbers) public onlyOperator(1) {
+        require(_accounts.length==_fnxnumbers.length,"the input array length is not equal");
+        uint256 i = 0;
+        for(;i<_accounts.length;i++) {
+            userWhiteList[_accounts[i]] = _fnxnumbers[i];
+            totalWhiteListAirdrop = totalWhiteListAirdrop.add(_fnxnumbers[i]);
+            emit AddWhiteList(_accounts[i],_fnxnumbers[i]);
+        }
     }
     
     
-    function claim() external {
-        require(optionColPool!=address(0),"collateral pool address should be set");
-        require(minePool!=address(0),"mine pool address should be set");
-        require(fnxToken!=address(0),"fnx token address should be set");
-        require(ftpbToken!=address(0),"ftpb token address should be set");
-        require(airDropWhiteList[msg.sender]>0,"claimer should be in whitelist");
-
-        uint256 amount = airDropWhiteList[msg.sender];
-        airDropWhiteList[msg.sender] = 0;
-        totalClaimed = totalClaimed.add(amount);
+    function whitelistClaim() external airdropinited {
+        require(userWhiteList[msg.sender]==0,"user balance is not enough");
+        uint256 amount = userWhiteList[msg.sender];
+        userWhiteList[msg.sender] = 0;
+        totalWhiteListClaimed = totalWhiteListClaimed.add(amount);
+        require(totalWhiteListClaimed<=maxWhiteListFnxAirDrop,"total claim amount over max whitelist airdrop");
+        
         IERC20(fnxToken).approve(optionColPool,amount);
         uint256 prefptb = IERC20(ftpbToken).balanceOf(address(this));
         IOptionMgrPoxy(optionColPool).addCollateral(fnxToken,amount);
         uint256 afterftpb = IERC20(ftpbToken).balanceOf(address(this));
         uint256 ftpbnum = afterftpb.sub(prefptb);
         IERC20(ftpbToken).approve(minePool,ftpbnum);
+        
         IMinePool(minePool).lockAirDrop(msg.sender,ftpbnum);
 
-        emit ClaimAirdrop(msg.sender,amount,ftpbnum);
+        emit WhiteListClaim(msg.sender,amount,ftpbnum);
     }
     
+
+    function freeClaim(address _targetToken) external airdropinited {
+        require(tokenWhiteList[_targetToken],"the target token is in token whitelist");
+        require(now >= claimBeginTime,"claim not begin");
+        require(now < claimEndTime,"claim finished");
+        require(!freeClaimedUserList[msg.sender],"user claimed airdrop already");
+  
+        
+        uint256 bal = ITargetToken(_targetToken).balanceOf(msg.sender);
+        require(bal>minBalForFreeClaim,"user balance in target token is not reach minum require");
+        
+        totalFreeClaimed = totalFreeClaimed.add(fnxPerFreeClaimUser);
+        require(totalFreeClaimed<=maxFreeFnxAirDrop,"total claim amount over max whitelist airdrop");
+        
+        IERC20(fnxToken).approve(optionColPool,fnxPerFreeClaimUser);
+        
+        uint256 prefptb = IERC20(ftpbToken).balanceOf(address(this));
+        IOptionMgrPoxy(optionColPool).addCollateral(fnxToken,fnxPerFreeClaimUser);
+        uint256 afterftpb = IERC20(ftpbToken).balanceOf(address(this));
+        uint256 ftpbnum = afterftpb.sub(prefptb);
+        
+        IERC20(ftpbToken).approve(minePool,ftpbnum);
+        IMinePool(minePool).lockAirDrop(msg.sender,ftpbnum);
+
+        emit UserFreeClaim(msg.sender,fnxPerFreeClaimUser,ftpbnum);
+    }   
+    
+   
+   function addSushiMineList(address[] memory _accounts,uint256[] memory _fnxnumbers) public onlyOperator(1) {
+        require(_accounts.length==_fnxnumbers.length,"the input array length is not equal");
+        uint256 i = 0;
+        uint256 idx = (now - sushiMineStartTime)/sushimineInterval;
+        for(;i<_accounts.length;i++) {
+            require((!sushiMineRecord[idx][msg.sender]),"user's mine have been set already");
+            sushiMineRecord[idx][msg.sender] = true;
+            
+            suhiUserMineBalance[_accounts[i]] = suhiUserMineBalance[_accounts[i]].add(_fnxnumbers[i]);
+            sushiTotalMine = sushiTotalMine.add(_fnxnumbers[i]);
+            
+            emit AddSushiList(_accounts[i],_fnxnumbers[i]);
+        }
+    }
+    
+    function sushiMineClaim() external suhsimineinited {
+        require(suhiUserMineBalance[msg.sender]>0,"sushi mine balance is not enough");
+        
+        uint256 amount = suhiUserMineBalance[msg.sender];
+        suhiUserMineBalance[msg.sender] = 0;
+        
+        uint256 precfnx = IERC20(cfnxToken).balanceOf(address(this));
+        IERC20(cfnxToken).transfer(msg.sender,amount);
+        uint256 aftercfnc = IERC20(cfnxToken).balanceOf(address(this));
+        uint256 cfncnum = precfnx.sub(aftercfnc);
+        require(cfncnum==amount,"transfer balance is wrong");
+    }
+      
 }
--- contracts/Airdrop/AirdropVaultData.sol
+++ contracts/Airdrop/AirdropVaultData.sol
@@ -5,17 +5,39 @@ import "../modules/Operator.sol";
 
 contract AirDropVaultData is Operator {
 
-
+    
     address public optionColPool;
     address public minePool;
+    address public cfnxToken;
     address public fnxToken;
     address public ftpbToken;
-    uint256 public totalAirdrop;
-    uint256 public totalClaimed;
+    
+    uint256 public totalWhiteListAirdrop;
+    uint256 public totalWhiteListClaimed;
+    uint256 public totalFreeClaimed;
+    uint256 public maxWhiteListFnxAirDrop;
+    uint256 public maxFreeFnxAirDrop;
+    
+    uint256 public claimBeginTime;
+    uint256 public claimEndTime;
+    uint256 public fnxPerFreeClaimUser;
+    uint256 public minBalForFreeClaim;
+    
     //the user's locked total balance
-    mapping (address => uint256) public airDropWhiteList;
+    mapping (address => uint256) public userWhiteList;
+    mapping (address => bool)    public tokenWhiteList;
+    mapping (address => bool)    public freeClaimedUserList;
+    
+    uint256 public sushiTotalMine;
+    uint256 public sushiMineStartTime;
+    uint256 public sushimineInterval = 30 days;
+    mapping (address => uint256) public suhiUserMineBalance;
+    mapping (uint256=>mapping(address => bool)) sushiMineRecord;
     
     event AddWhiteList(address indexed claimer, uint256 indexed amount);
-    event ClaimAirdrop(address indexed claimer, uint256 indexed amount,uint256 indexed ftpbnum);
+    event WhiteListClaim(address indexed claimer, uint256 indexed amount,uint256 indexed ftpbnum);
+    event UserFreeClaim(address indexed claimer, uint256 indexed amount,uint256 indexed ftpbnum);
+    
+    event AddSushiList(address indexed claimer, uint256 indexed amount);
 
 }
