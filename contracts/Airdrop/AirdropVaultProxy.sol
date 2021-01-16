55459662536669da9abe5ee4b2b9b9241df92c5d
jeffqg123 <forestjqg@126.com>
2021-01-16 19:38:03 +0800

    remove comments

--- contracts/Airdrop/AirdropVaultProxy.sol
+++ contracts/Airdrop/AirdropVaultProxy.sol
@@ -2,11 +2,7 @@ pragma solidity =0.5.16;
 import "./AirdropVaultData.sol";
 import "../Proxy/baseProxy.sol";
 
-/**
- * @title FPTCoin is finnexus collateral Pool token, implement ERC20 interface.
- * @dev ERC20 token. Its inside value is collatral pool net worth.
- *
- */
+
 contract AirDropVaultProxy is AirDropVaultData,baseProxy {
     
     constructor (address implementation_) baseProxy(implementation_) public{
