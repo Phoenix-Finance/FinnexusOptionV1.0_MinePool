138e91a1a1e2471ea859f98a5db4ce11ae8bd644
jiaqinggang1 <forestjqg@126.com>
2021-01-28 01:50:15 -0800

    updated interface

--- contracts/Airdrop/PoolAdapter.sol
+++ contracts/Airdrop/PoolAdapter.sol
@@ -3,20 +3,18 @@ pragma solidity =0.5.16;
 import "../modules/Ownable.sol";
 
 interface IColPool {
-    function userInputCollateral(address user,address collateral) external view returns (uint256);
+    function getUserTotalWorth(address _account) external view returns (uint256);
 }
 
 contract PoolAdapter is Ownable{
     
-    address public optionColPool;
-    address public collateral;
-    
-    function setColPool(address _optionColPool,address _collateral) public onlyOwner {
+    address public optionColPool = 0x120f18F5B8EdCaA3c083F9464c57C11D81a9E549;
+ 
+    function setColPool(address _optionColPool) public onlyOwner {
         optionColPool = _optionColPool;
-        collateral = _collateral;
     }
     
     function balanceOf(address _account) external view returns (uint256){
-        return IColPool(optionColPool).userInputCollateral(_account,collateral);
+        return IColPool(optionColPool).getUserTotalWorth(_account);
     }
 }
